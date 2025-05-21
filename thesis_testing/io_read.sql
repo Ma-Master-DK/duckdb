5d9732c1eb19902181d7411de6ea64eab0f4acc4
Marius <business@mottosen.xyz>
2025-05-21 16:07:15 +0200

    Cleaned up a bit, and new test setup. We now read/write seperately for each build, and generate plots showing average timings.

--- thesis_testing/io_read.sql
+++
@@ -1,1 +0,0 @@
-EXPLAIN ANALYZE SELECT COUNT(*), SUM(LENGTH(payload)) FROM io_test;
--- thesis_testing/io_write.sql
+++
@@ -1,6 +0,0 @@
-EXPLAIN ANALYZE INSERT INTO io_test
-SELECT r as id,
-        repeat('abcdefghij', 100) as payload       -- ~1KB payload per row
-FROM range(1, 10 * 1024 * 1024 + 1) as t(r);    -- 10 million rows
-
-CHECKPOINT;
