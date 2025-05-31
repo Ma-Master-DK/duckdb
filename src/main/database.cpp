8c946a64b615dc58908081835470bb184f2c0bb3
Marius <business@mottosen.xyz>
2025-05-31 17:25:04 +0200

    Fix for char devices.

--- src/main/database.cpp
+++ src/main/database.cpp
@@ -417,7 +417,7 @@ void DatabaseInstance::Configure(DBConfig &new_config, const char *database_path
 
 		xnvme_opts opts = xnvme_opts_default();
 		opts.be = "linux";
-		opts.async = "io_uring";
+		opts.async = "io_uring_cmd";
 		config.options.dev = xnvme_dev_open(database_path, &opts);
 		if (!config.options.dev) {
 			xnvme_cli_perr("xnvme_dev_open()", errno);
