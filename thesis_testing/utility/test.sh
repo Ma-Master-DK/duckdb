0d6df306c74e06988d296197272708c701f30cf7
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2025-06-09 11:33:04 +0200

    Add more sfs

--- thesis_testing/utility/test.sh
+++ thesis_testing/utility/test.sh
@@ -12,7 +12,7 @@ NVME_ASYNC_MQUEUE="../build/duckdb_xnvme_async_mqueue"
 NVME_ASYNC_TQUEUE="../build/duckdb_xnvme_async_tqueue"
 NVME_ASYNC_TQUEUE_NO_PASSTHROUGH="../build/duckdb_xnvme_async_tqueue_no_passthrough"
 
-sfs=(0.01 0.1 1 2 4 6 8 10 20 40 60 80 100)
+sfs=(0.01 0.1 1 2 4 6 8 10 20 40 60 80 100 200 300)
 RUNS=50
 RESULTS="../results/test_results.txt"
 
